5dca5d7a98610a75a5c323d1e5927dd62611de48
joshua989 <josipoli0222@gmail.com>
2025-07-31 12:26:03 +0100

    paymnet

--- fashionista/settings.py
+++ fashionista/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-import os
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,8 +153,3 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5501",  # Your frontend origin
     "http://localhost:5500",   # Alternative
 ]
-
-
-load_dotenv()
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
